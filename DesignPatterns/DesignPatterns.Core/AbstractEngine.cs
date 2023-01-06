7deac834672060373a803d78e69a65e7977fe246
Santhosh Reddy Vootukuri (SUNNY) <nagavo@microsoft.com>
2023-01-06 08:49:43 -0800

    First commit to the repository

---
+++ DesignPatterns/DesignPatterns.Core/AbstractEngine.cs
@@ -0,0 +1,25 @@
+﻿namespace DesignPatterns.Core
+{
+    /// <summary>
+    /// Abstract class implementing IEngine
+    /// </summary>
+    public abstract class AbstractEngine : IEngine
+    {
+        private int size;
+        private bool turbo;
+
+        protected AbstractEngine(int size, bool turbo)
+        {
+            this.size = size;
+            this.turbo = turbo;
+        }
+
+        public virtual int Size { get => size;}
+        public virtual bool Turbo { get => turbo; }
+
+        public override string ToString()
+        {
+            return this.GetType().Name + $"({size})";
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatterns.Core/IEngine.cs
@@ -0,0 +1,11 @@
+﻿namespace DesignPatterns.Core
+{
+    /// <summary>
+    /// Interface for Engine object
+    /// </summary>
+    public interface IEngine
+    {
+        int Size { get;}
+        bool Turbo { get;}
+    }
+}
