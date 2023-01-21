b7667c8946164e6e983f9318fd5fdd6147ec788f
Santhosh Reddy Vootukuri (SUNNY) <nagavo@microsoft.com>
2023-01-20 16:14:09 -0800

    Composite pattern

--- DesignPatterns/DesignPatterns.Structural.Composite/Program.cs
+++ DesignPatterns/DesignPatterns.Structural.Composite/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("____________________________________________________________________________________");
+Console.WriteLine("Bridge: Decouple an abstraction from its implementation so that each may vary independently");
+Console.WriteLine("____________________________________________________________________________________");
+Console.WriteLine("\n");
+
