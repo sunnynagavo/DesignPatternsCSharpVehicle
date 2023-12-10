ded1e57489c9af2417d42d00e9d28fb7f93c3c64
Santhosh Reddy Vootukuri (SUNNY) <nagavo@microsoft.com>
2023-12-10 13:42:36 -0800

    Updated dotnet version to core 8.0

--- DesignPatterns/DesignPatterns.CreationalPaterns.Builder/CarBuilder.cs
+++ DesignPatterns/DesignPatterns.CreationalPaterns.Builder/CarBuilder.cs
@@ -11,12 +11,12 @@ namespace DesignPatterns.CreationalPaterns.Builder
         {
             this.carInProgress = carInProgress;
         }
+        
 
         public override void BuildBody() { Console.WriteLine("Building Car Body"); }
         public override void BuildBoot() { Console.WriteLine("Building Car Boot"); }
         public override void BuildChasis() { Console.WriteLine("Building Car Chasis"); }
         public override void BuildPassengerArea() { Console.WriteLine("Building Car Passenger Area"); }
-       
         // commented because this method is not applicable to cars
         // public override void BuildReinforcedStorageArea() { Console.WriteLine("Building Car Body"); }
         public override void BuildWindows() { Console.WriteLine("Building Car Windows"); }
